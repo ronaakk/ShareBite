9eb2711415a4020defdb5c7ec33e18088baccb64
hpuranik <harshapuranik1704@gmail.com>
2024-11-08 18:41:50 -0500

    Added authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Link from "next/link";
 import Image from "next/image";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import  NavBar from "@/components/landingPage/NavBar"
--- src/components/landingPage/NavBar.jsx
+++ src/components/landingPage/NavBar.jsx
@@ -1,5 +1,7 @@
 "use client"
 
+import Link from "next/link";
+
 function NavBar() {
   return (
     <div className="relative flex flex-wrap items-center justify-between w-full bg-white group py-7 shrink-0">
@@ -7,10 +9,13 @@ function NavBar() {
         {/* TODO: Logo will go here */}
         <p className="logo_text">ShareBite</p>
         </div>
-        <div className="items-center hidden gap-8 md:flex">
-            <button className="outline_btn">Log In</button>
-            <button className="flex items-center px-4 py-2 text-sm rounded-xl bg-light-green hover:bg-primary-green hover:font-bold hover:text-black transition duration-300">
-                Sign Up
+        <div className="items-center hidden gap-6 md:flex">
+            <button className="flex items-center px-4 py-2 text-sm rounded-xl font-bold hover:bg-light-green">
+              <Link href="api/auth/login">Log In</Link>
+            </button>
+            <button className="flex items-center px-4 py-2 text-sm rounded-xl bg-light-green hover:bg-primary-green font-bold hover:text-black transition duration-300">
+              <Link href="api/auth/logout">Logout</Link>
+              {/* Change this to sign up or remove */}
             </button>
         </div>
     </div>
